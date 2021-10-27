0398506b40e82b479211ed8cf2695eaeb785e9d8
guton <guton@LAPTOP-JKIEQ0B6>
2021-10-27 13:42:46 -0300

    Adicionar arquivos de projeto.

---
+++ AppCronogramaAula/Model/Conexao.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace AppCronogramaAula.Model
+
+{
+    class Conexao
+    {
+        public static string conectar()
+        {
+            return @"C:\USERS\GUTON\DESKTOP\APPCRONOGRAMAAULA\APPCRONOGRAMAAULA\BDCRONOGRAMA_AULA.MDF";
+        }
+    }
+}
